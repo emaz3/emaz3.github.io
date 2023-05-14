a39c75f2ca1399df3edaa1e06de878695a9e39a6
emaz3 <109099979+tf2client@users.noreply.github.com>
2023-05-14 17:09:14 -0400

    update

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const randomIndex = Math.floor(Math.random() * logoUrls.length);
 
     logo.src = logoUrls[randomIndex];
+
+    // Redirect button click event
+    const redirectButton = document.getElementById('redirect-button');
+    redirectButton.addEventListener('click', () => {
+        window.location.href = 'https://workupload.com/file/YknTkCR2nQA';
+    });
 });
