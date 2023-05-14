b5cb16910d95f8185eb5e21d3fbbc1d9075d4a46
emaz3 <109099979+tf2client@users.noreply.github.com>
2023-05-14 17:25:21 -0400

    chnages removed logo

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const logo = document.getElementById('logo');
-    const logoUrls = [
-        'logo1.png'
-    ];
-
-    const randomIndex = Math.floor(Math.random() * logoUrls.length);
-
-    logo.src = logoUrls[randomIndex];
-
-    // Redirect button click event
     const redirectButton = document.getElementById('redirect-button');
     redirectButton.addEventListener('click', () => {
         window.location.href = 'https://workupload.com/file/YknTkCR2nQA';
