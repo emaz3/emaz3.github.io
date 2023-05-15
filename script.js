301a03f95f75109afcc2a3632534d91dc485e567
emaz3 <109099979+tf2client@users.noreply.github.com>
2023-05-15 16:46:43 -0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const redirectButton = document.getElementById('redirect-button');
     redirectButton.addEventListener('click', () => {
-        window.location.href = 'https://workupload.com/file/YknTkCR2nQA';
+        window.open('https://workupload.com/file/YknTkCR2nQA', '_blank');
     });
 });
