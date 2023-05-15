abff0b470cb0a66468b3729ca7292c91e71b0d71
emaz3 <andrewturth@gmail.com>
2023-05-15 17:17:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,48 @@ window.addEventListener('DOMContentLoaded', (event) => {
     redirectButton.addEventListener('click', () => {
         window.open('https://workupload.com/file/YknTkCR2nQA', '_blank');
     });
+
+    const tabTitle = document.title;
+    const text = "[emaz3]";
+    const typingSpeed = 100;
+    const pauseDuration = 500;
+
+    function typeEffect() {
+        document.title = "";
+
+        let i = 0;
+        let isDeleting = false;
+
+        function typeLoop() {
+            setTimeout(() => {
+                if (isDeleting) {
+                    document.title = tabTitle.substring(0, i - 1);
+                    i--;
+                } else {
+                    document.title = tabTitle + text.substring(0, i + 1);
+                    i++;
+                }
+
+                if (isDeleting && i === 0) {
+                    isDeleting = false;
+                }
+
+                if (!isDeleting && i === text.length) {
+                    isDeleting = true;
+                }
+
+                if (isDeleting) {
+                    typeLoop();
+                } else {
+                    setTimeout(typeLoop, typingSpeed);
+                }
+            }, typingSpeed);
+        }
+
+        setTimeout(() => {
+            typeLoop();
+        }, pauseDuration);
+    }
+
+    typeEffect();
 });
