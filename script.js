5f2732281f02d5205b1299e122618b4c994ef392
emaz3 <andrewturth@gmail.com>
2023-05-16 19:59:52 -0400

    sadf

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const tabTitle = document.title;
     const text = " [ e m a z 3 ] ";
     const typingSpeed = 150;
-    const pauseDuration = 50;
+    const pauseDuration = 0;
 
     function typeEffect() {
         let i = 0;
