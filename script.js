1be3b2eea071582b10ebcf2c882fb4060c2368ba
emaz3 <andrewturth@gmail.com>
2023-05-18 20:37:43 -0400

    hexagon and like particle

--- script.js
+++ script.js
@@ -18,48 +18,120 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 
     startRain();
-});
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const tabTitle = document.title;
-    const text = " [ e m a z 3 ] ";
-    const typingSpeed = 150;
-    const pauseDuration = 0;
+    // particle
+    const canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
 
-    function typeEffect() {
-        let i = 0;
-        let isDeleting = false;
+    const ctx = canvas.getContext('2d');
+    const particles = [];
+    const particleCount = 50;
+    const maxDistance = 100;
+    const fenceSize = 200;
+    let mouseX = 0;
+    let mouseY = 0;
 
-        function typeLoop() {
-            setTimeout(() => {
-                if (isDeleting) {
-                    document.title = tabTitle.substring(0, i - 1) + text;
-                    i--;
-                } else {
-                    document.title = tabTitle + text.substring(0, i + 1);
-                    i++;
-                }
+    function Particle(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 3 + 1;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
 
-                if (isDeleting && i === 0) {
-                    isDeleting = false;
-                }
+        this.update = () => {
+            this.x += this.speedX;
+            this.y += this.speedY;
 
-                if (!isDeleting && i === text.length) {
-                    isDeleting = true;
-                }
+            if (this.x < -fenceSize || this.x > canvas.width + fenceSize || this.y < -fenceSize || this.y > canvas.height + fenceSize) {
+                this.x = Math.random() * canvas.width;
+                this.y = Math.random() * canvas.height;
+            }
 
-                if (isDeleting) {
-                    typeLoop();
-                } else {
-                    setTimeout(typeLoop, typingSpeed);
-                }
-            }, typingSpeed);
-        }
+            this.draw();
+        };
 
-        setTimeout(() => {
-            typeLoop();
-        }, pauseDuration);
+        this.draw = () => {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fillStyle = '#ffffff';
+            ctx.fill();
+        };
     }
 
-    typeEffect();
+    function connectDots() {
+        for (let i = 0; i < particles.length; i++) {
+            for (let j = i + 1; j < particles.length; j++) {
+                const dx = particles[i].x - particles[j].x;
+                const dy = particles[i].y - particles[j].y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < maxDistance) {
+                    ctx.beginPath();
+                    ctx.strokeStyle = '#ffffff';
+                    ctx.lineWidth = (1 - distance / maxDistance) * 2;
+                    ctx.moveTo(particles[i].x, particles[i].y);
+                    ctx.lineTo(particles[j].x, particles[j].y);
+                    ctx.stroke();
+                    ctx.closePath();
+                }
+            }
+        }
+    }
+
+    function createParticles() {
+        for (let i = 0; i < particleCount; i++) {
+            const x = Math.random() * canvas.width;
+            const y = Math.random() * canvas.height;
+            particles.push(new Particle(x, y));
+        }
+    }
+
+    function updateParticles() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < particles.length; i++) {
+            const particle = particles[i];
+
+            // direction to mouse
+            const dx = mouseX - particle.x;
+            const dy = mouseY - particle.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            const directionX = dx / distance;
+            const directionY = dy / distance;
+
+            // particle pos
+            particle.x += directionX;
+            particle.y += directionY;
+
+            particle.update();
+        }
+
+        connectDots();
+
+        requestAnimationFrame(updateParticles);
+    }
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    function updateMousePosition(event) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    }
+
+    function addEventListeners() {
+        window.addEventListener('resize', resizeCanvas);
+        window.addEventListener('mousemove', updateMousePosition);
+    }
+
+    function initialize() {
+        resizeCanvas();
+        createParticles();
+        addEventListeners();
+        updateParticles();
+    }
+
+    initialize();
 });
